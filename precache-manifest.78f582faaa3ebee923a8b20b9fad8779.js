b127934529b6908b36750b4b985297284259387f
MsUliana <ulianadzoba@gmail.com>
2020-01-26 23:09:27 +0200

    Updates

---
+++ precache-manifest.78f582faaa3ebee923a8b20b9fad8779.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c01db16400be2ac7db5395aadbadfa9",
+    "url": "/weather_forecast/index.html"
+  },
+  {
+    "revision": "7898167043e5666e74c6",
+    "url": "/weather_forecast/static/css/main.2b9d812e.chunk.css"
+  },
+  {
+    "revision": "2e4447762cda809fc03f",
+    "url": "/weather_forecast/static/js/2.17bef1ec.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/weather_forecast/static/js/2.17bef1ec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7898167043e5666e74c6",
+    "url": "/weather_forecast/static/js/main.0ea3d65a.chunk.js"
+  },
+  {
+    "revision": "a31b5053f5f96e0ee1cf",
+    "url": "/weather_forecast/static/js/runtime-main.ffbb6931.js"
+  },
+  {
+    "revision": "0167f510d6a676adaec807e70ac3c79a",
+    "url": "/weather_forecast/static/media/bg.0167f510.jpg"
+  }
+]);
